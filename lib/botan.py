0af71caccc51c653e010081362da3330753c5294
Kverde <Kverde@yandex.ru>
2017-01-15 12:09:14 +0500

    fix botan

--- lib/botan.py
+++ lib/botan.py
@@ -21,7 +21,6 @@ def track(token, uid, message, name='Message'):
             data=json.dumps(message),
             headers={'Content-type': 'application/json'},
         )
-        print(r.json())
         return r.json()
     except requests.exceptions.Timeout:
         # set up for a retry, or continue in a retry loop
